053f8a34a9237e7029def2e63fdf9ba858eca8ad
Peterson William <petersondev.profissional@gmail.com>
2024-02-28 16:46:50 -0300

    Create Menu.java

---
+++ src/sistema/Menu.java
@@ -0,0 +1,20 @@
+package sistema;
+
+public class Menu {
+
+    public static void menuInicial() {
+        System.out.println("===== MENU INICIAL =====");
+        System.out.println("1 - ACESSAR MINHA CONTA");
+        System.out.println("2 - CRIAR UMA CONTA");
+        System.out.println("3 - SAIR");
+    }
+
+    public static void menuConta() {
+        System.out.println("===== BEM-VINDO =====");
+        System.out.println("1 - DEPOSITAR DINHEIRO");
+        System.out.println("2 - SACAR DINHEIRO");
+        System.out.println("3 - TRANSFERÊNCIA PARA OUTRA CONTA");
+        System.out.println("4 - CONSULTAR MEUS DADOS");
+        System.out.println("5 - EXCLUIR MINHA CONTA");
+    }
+}
