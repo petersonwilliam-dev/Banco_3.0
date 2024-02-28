047c9a8a420821f888d4c9bae6e9bcb5d391c0e1
Peterson William <petersondev.profissional@gmail.com>
2024-02-28 16:16:33 -0300

    Create TitularConta.java

---
+++ src/entidades/TitularConta.java
@@ -0,0 +1,56 @@
+package entidades;
+
+import java.util.Objects;
+
+public class TitularConta {
+    private int id;
+    private String nome;
+    private String cpf;
+    private String numero_telefone;
+
+    public TitularConta(String nome, String cpf, String numero_telefone) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.numero_telefone = numero_telefone;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getNumero_telefone() {
+        return numero_telefone;
+    }
+
+    public void setNumero_telefone(String numero_telefone) {
+        this.numero_telefone = numero_telefone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TitularConta that)) return false;
+        return  Objects.equals(getCpf(), that.getCpf());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome(), getCpf(), getNumero_telefone());
+    }
+}
